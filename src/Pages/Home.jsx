1128081fa6a008118e70daefd5ef853d40f1ac97
Md. Arshi Khan <arshikhan80708@gmail.com>
2025-07-17 17:35:50 +0530

    modified File Structure

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Carousel from "../Components/Ui/Carousel";
+import About from "../Components/section/About";
+
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
