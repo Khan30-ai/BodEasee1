5c73839fb439f4c53a15207084d136faea99f1d4
Md. Arshi Khan <arshikhan80708@gmail.com>
2025-07-19 09:37:16 +0530

    Added Service

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import Carousel from "../Components/Ui/Carousel";
 import About from "../Components/Section/About";
 import Program from "../Components/Section/Program";
+import Service from "../Components/Section/Service";
 
 const Home = () => {
   return (
     <div>
-      <Carousel />
-      <About />
+      <section id="home">
+        <Carousel />
+      </section>
+      <section id="about">
+        <About />
+      </section>
       <Program />
+      <Service />
     </div>
   );
 };
