51082300dcd57361cef6a8269c2b811dd168f878
Md. Arshi Khan <arshikhan80708@gmail.com>
2025-07-20 22:00:25 +0530

    Created path for all programs

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,20 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
-import Home from "./Pages/Home.jsx";
+import "@/index.css";
+import App from "@/App.jsx";
+import Home from "@/Pages/Home.jsx";
+import WeightLoss from "@/Pages/WeightLoss";
+import WeightGain from "@/Pages/WeightGain";
+import Skin from "@/Pages/Skin";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Home />} />
+      <Route path="/programs/weight-loss" element={<WeightLoss />} />
+      <Route path="/programs/weight-gain" element={<WeightGain />} />
+      <Route path="/programs/skin-hair" element={<Skin />} />
     </Route>
   )
 );
