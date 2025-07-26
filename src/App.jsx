4a851d92e907c1492256d7f089efbf6d814c726f
Md. Arshi Khan <arshikhan80708@gmail.com>
2025-07-26 21:36:45 +0530

    Imported motion and AnimatePresence for a smoooth experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "./Components/Header/Navbar";
 import Footer from "./Components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <Navbar />
-      <Outlet />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          <Outlet />
+        </motion.div>
+      </AnimatePresence>
+
       <Footer />
     </>
   );
